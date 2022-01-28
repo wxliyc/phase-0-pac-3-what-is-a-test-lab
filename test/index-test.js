6a14e891e49bef998f412bf8addc75b4750e9016
Ethan Lee <wxliyc@gmail.com>
2022-01-28 13:41:41 -0600

    commit tree cb3440cf1db0140287da2e638d71f703cf808689

--- test/index-test.js
+++ test/index-test.js
@@ -9,7 +9,7 @@ describe("what-is-a-test", () => {
 
   describe("Height", () => {
     it("is less than 40", () => {
-      expect(height).toBeLessThan(40);
+      expect(39).toBeLessThan(40);
     });
   });
 
